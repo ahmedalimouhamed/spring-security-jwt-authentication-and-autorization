3b6c8cbe92cfe951618bd1422731ed34633a7836
zerzif-chouaib-git <zerzif.bouchaib@gmail.com>
2023-07-22 12:05:10 +0100

    commit 22-07-23 12:05

---
+++ src/main/java/com/workshop/ahmed/AhmedApplication.java
@@ -0,0 +1,11 @@
+package com.workshop.ahmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhmedApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AhmedApplication.class, args);
+	}
+}
---
+++ src/main/java/com/workshop/ahmed/user/Role.java
@@ -0,0 +1,6 @@
+package com.workshop.ahmed.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
